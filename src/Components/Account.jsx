69f1c07cedb044dcf884a66805d32320661e6d92
Mathew Dewes <mathewdewes@hotmail.com>
2023-10-08 17:55:40 +1300

    Add delete item

--- src/Components/Account.jsx
+++ src/Components/Account.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import testImage from "../images/matt.JPG";
+
 import {
   collection,
   query,
@@ -7,27 +8,47 @@ import {
   getDocs,
   updateDoc,
   doc,
+  deleteDoc
 } from "firebase/firestore";
+
 import { db } from "../firebase-config";
 
 export default function Account(props) {
   const [listings, setListings] = useState([]);
 
+  const handleDelete = async (id)=>{
+const listingRef = doc(db, "Items", id)
+const deletedListing = await deleteDoc(listingRef)
+setListings(prevListings=>prevListings.filter(item=>item.id !==id))
+  }
+
   const handleEdit = async (name, price, id) => {
+    console.log("HERE", name, price, id);
+
     const listingRef = doc(db, "Items", id);
-    await updateDoc(listingRef, {
+
+    const listingReq = await updateDoc(listingRef, {
       name: name,
+
       price: price,
     });
-    const updatedListings = listings.map((listing)=>{
-    if (listing.id == id){
-      return {
-        ...listing, 
-        name:name,
-        price:price
+
+    const updatedListings = listings.map((listing) => {
+      if (listing.id == id) {
+        return {
+          ...listing,
+
+          name: name,
+
+          price: price,
+        };
       }
-    }
-    })
+
+      return listing;
+    });
+
+    console.log("HERE UPDATED LISTING", updatedListings);
+
     setListings(updatedListings);
   };
 
@@ -37,31 +58,40 @@ export default function Account(props) {
 
       const q = query(
         accountRef,
+
         where("user_id", "==", props.userDetails.userCredential)
       );
 
       const querySnapShot = await getDocs(q);
+
       return querySnapShot.docs.map((doc) => ({
         id: doc.id,
 
         ...doc.data(),
       }));
     }
+
     getUserLisitngs().then((listings) => {
       setListings(listings);
     });
   }, []);
+
   console.log(listings);
+
   return (
     <div>
       <h1 className="pageHeading">Account</h1>
+
       <div className="account">
         <div className="profile">
           <img src={testImage} alt="" />
+
           <div>
             <h2>{props.userDetails.name}</h2>
+
             <p>Balance: 2</p>
           </div>
+
           <div>
             <p>
               Bio: Lorem ipsum dolor sit, amet consectetur adipisicing elit.
@@ -69,13 +99,15 @@ export default function Account(props) {
             </p>
           </div>
         </div>
+
         <div></div>
 
         <h1>My Listings:</h1>
+
         <div className="grid">
           {listings && listings.length > 0 ? (
             listings.map((listing) => {
-              return <Listing handleEdit={handleEdit} listing={listing} />;
+              return <Listing handleDelete={handleDelete} handleEdit={handleEdit} listing={listing} />;
             })
           ) : (
             <></>
@@ -88,26 +120,43 @@ export default function Account(props) {
 
 const Listing = (props) => {
   const [name, setName] = useState("");
+
   const [price, setPrice] = useState("");
+
   const [isEdit, setIsEdit] = useState(false);
+
   if (!isEdit) {
     return (
       <div className="grid__item">
-        <h2>{props.listing.name}</h2>
+        <h2>{props.listing ? props.listing.name : ""}</h2>
 
-        <p>{props.listing.price}</p>
+        <p>{props.listing ? props.listing.price : ""}</p>
+
         <button onClick={() => setIsEdit(true)}>Edit</button>
-        <button>Delete</button>
+
+        <button onClick={()=>props.handleDelete(props.listing.id)}>Delete</button>
       </div>
     );
   } else {
     return (
       <div className="grid__item">
         <h2>{props.listing.name}</h2>
-        <form onSubmit={()=>props.handleEdit(name, price, props.listing.id)}>
-          <input onChange={(e)=>setName(e.target.value)} placeholder={props.listing.name} type="text" />
-          <input onChange={(e)=>setPrice(e.target.value)} placeholder={props.listing.price} type="number" />
+
+        <form onSubmit={() => props.handleEdit(name, price, props.listing.id)}>
+          <input
+            onChange={(e) => setName(e.target.value)}
+            placeholder={props.listing.name}
+            type="text"
+          />
+
+          <input
+            onChange={(e) => setPrice(e.target.value)}
+            placeholder={props.listing.price}
+            type="number"
+          />
+
           <button type="submit">Edit</button>
+
           <button onClick={() => setIsEdit(false)}>Close</button>
         </form>
       </div>
